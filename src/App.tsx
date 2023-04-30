0e509e15ca82ee01a8aaf448c31be2e6625d5413
Vytenis <vytenis.kondrackis@gmail.com>
2023-05-01 00:41:03 +0300

    testing env file 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Browse } from './pages/browse/Browse';
 import { Login } from './pages/login/Login';
 import { Register } from './pages/register/Register';
 import { Details } from './pages/details/Details';
-
+import dotenv from 'dotenv';
 import { Player } from './pages/player/Player';
 import { Library } from './pages/library/Library';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -13,6 +13,7 @@ import { Home } from './pages/home/Home';
 
 function App() {
   const queryClient = new QueryClient();
+  dotenv.config();
   return (
     <div className='app'>
       <QueryClientProvider client={queryClient}>
