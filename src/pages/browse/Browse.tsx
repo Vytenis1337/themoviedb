b03c0701d97ec37b7a0a1ca522f9278778a590b9
Vytenis <vytenis.kondrackis@gmail.com>
2023-05-02 11:07:16 +0300

    added process env for axios link

--- src/pages/browse/Browse.tsx
+++ src/pages/browse/Browse.tsx
@@ -14,7 +14,7 @@ export const Browse = () => {
       axios
         .get(
           `
-          https://api.themoviedb.org/3/movie/top_rated?api_key=ab4acdde934f536f88f60bc1bf46f441&language=en-US&page=1`
+          https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
         )
         .then((res) => {
           return res.data.results;
