06b6f9168118c15252c7a697a8b8cee5dfe9f42d
Vytenis <vytenis.kondrackis@gmail.com>
2023-05-03 08:54:01 +0300

    deleted unused global context component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import { Browse } from './pages/browse/Browse';
 import { Login } from './pages/login/Login';
 import { Register } from './pages/register/Register';
 import { Details } from './pages/details/Details';
-
 import { Player } from './pages/player/Player';
 import { Library } from './pages/library/Library';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { GlobalProvider } from './reducers/GlobalState';
+
 import { Home } from './pages/home/Home';
 import { Contacts } from './pages/contacts/Contacts';
 
@@ -18,21 +17,19 @@ function App() {
   return (
     <div className='app'>
       <QueryClientProvider client={queryClient}>
-        <GlobalProvider>
-          <BrowserRouter>
-            <Navbar />
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/browse' element={<Browse />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/movies/single/:id' element={<Details />} />
-              <Route path='/player/:id' element={<Player />} />
-              <Route path='/library' element={<Library />} />
-              <Route path='/contacts' element={<Contacts />} />
-            </Routes>
-          </BrowserRouter>
-        </GlobalProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/browse' element={<Browse />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/movies/single/:id' element={<Details />} />
+            <Route path='/player/:id' element={<Player />} />
+            <Route path='/library' element={<Library />} />
+            <Route path='/contacts' element={<Contacts />} />
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
     </div>
   );
