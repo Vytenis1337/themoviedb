d8dc74462910bd765b7ed5e62ad2fe36c66c0d6d
Vytenis <vytenis.kondrackis@gmail.com>
2023-05-03 13:35:32 +0300

    changed back to localhost baseURL for testing

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const newRequest = axios.create({
-  baseURL: 'https://themoviedb-backend.onrender.com/',
+  baseURL: 'http://localhost:8800/api/',
   withCredentials: true,
 });
 
