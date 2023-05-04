2d4babf150ba2d5f5439b15284a532a811411ad2
Vytenis <vytenis.kondrackis@gmail.com>
2023-05-04 16:35:58 +0300

    testing baseURL: added crossDomain to axios.create

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const newRequest = axios.create({
   baseURL: 'https://themoviedb-backend.onrender.com/',
   withCredentials: true,
+  crossDomain: true,
 });
 
 export default newRequest;
